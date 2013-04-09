176b0a952938ad7a696ae11d8c0fbcc95be289ca
sorin.sbarnea@citrix.com <sorin.sbarnea@citrix.com>
2013-04-09 16:21:54 +0100

    added setenv.sh config files for confluence, jira and crowd which enable RMI on default port+1

---
+++ opt/Confluence/bin/setenv.sh
@@ -0,0 +1,41 @@
+JAVA_OPTS="-Xms256m -Xmx2512m -XX:MaxPermSize=512m $JAVA_OPTS -Djava.awt.headless=true \
+-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8091 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
+export JAVA_OPTS
+
+echo "If you encounter issues starting up Confluence Standalone, please see the Installation guide at http://confluence.atlassian.com/display/DOC/Confluence+Installation+Guide"
+
+# set the location of the pid file
+if [ -z "$CATALINA_PID" ] ; then
+    if [ -n "$CATALINA_BASE" ] ; then
+        CATALINA_PID="$CATALINA_BASE"/work/catalina.pid
+    elif [ -n "$CATALINA_HOME" ] ; then
+        CATALINA_PID="$CATALINA_HOME"/work/catalina.pid
+    fi
+fi
+export CATALINA_PID
+
+PRGDIR=`dirname "$0"`
+if [ -z "$CATALINA_BASE" ]; then
+  if [ -z "$CATALINA_HOME" ]; then
+    LOGBASE=$PRGDIR
+    LOGTAIL=..
+  else
+    LOGBASE=$CATALINA_HOME
+    LOGTAIL=.
+  fi
+else
+  LOGBASE=$CATALINA_BASE
+  LOGTAIL=.
+fi
+
+PUSHED_DIR=`pwd`
+cd $LOGBASE
+cd $LOGTAIL
+LOGBASEABS=`pwd`
+cd $PUSHED_DIR
+
+echo ""
+echo "Server startup logs are located in $LOGBASEABS/logs/catalina.out"
+
+#JRE_HOME="/opt/Confluence/jre/"; export JRE_HOME
